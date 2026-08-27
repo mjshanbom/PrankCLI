54dafe137ad35968101b5a01aad9af4a96ba1736
Matthew Shanbom <matthew.shanbom@jmfamily.com>
2026-08-27 16:01:16 -0400

    final

--- Warmup/obj/Debug/net10.0-windows10.0.19041.0/Warmup.AssemblyInfo.cs
+++ Warmup/obj/Debug/net10.0-windows10.0.19041.0/Warmup.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Warmup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc5184281fe20197be5be0bcfbdcff2cfe1d50c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1b3519263339d41a5da53de8277e2121d12a1c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Warmup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Warmup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
